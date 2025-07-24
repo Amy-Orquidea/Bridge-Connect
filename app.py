d01524b6b86b7f80e226b31c50a522005a7d1acc
Amy <amy.orquidea57@gmail.com>
2025-07-24 15:47:33 -0300

    atualização do código python, com excluir

--- app.py
+++ app.py
@@ -5,33 +5,44 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///meubanco.db'
 db = SQLAlchemy(app)
 
-with app.app_context():
-    db.create_all()
-
-
 class Usuarios(db.Model):
-    email = db.Column(db.String(50), nullable=False, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    email = db.Column(db.String(50), nullable=False)
     nome = db.Column(db.String(80), nullable=False)
     lingua = db.Column(db.String(20), nullable=False)
     senha = db.Column(db.String(15), nullable=False)
     telefone = db.Column(db.String(10), nullable=False)
-    endereco = db.Column(db.String(120), nullable=False)
-    advogado_email = db.Column(db.String(50), db.ForeignKey('advogados.email'), nullable=True)
-    advogado = db.relationship('Advogados', backref='usuarios')
-    ong_id = db.Column(db.Integer, db.ForeignKey('ong.id'), nullable=True)
-    ong = db.relationship('Ongs', backref='usuarios')    
-      
-class Advogados(db.model):
-    email = db.Column(db.String(50), nullable=False, primary_key=True)
+    endereco = db.Column(db.String(120), nullable=False)   
+
+class Usuarios_Ongs(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    ong_id = db.Column(db.Integer, db.ForeignKey('ongs.id'))
+    ong = db.relationship('Ongs', backref='usuarios_ongs') 
+    usuarioId = db.Column(db.Integer, db.ForeignKey('usuarios.id'))
+    usuario = db.relationship('Usuarios', backref='usuarios_ongs')
+    
+class Usuarios_Advogados(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    advogadoId = db.Column(db.Integer, db.ForeignKey('advogados.id'))
+    advogado = db.relationship('Advogados', backref='usuarios_advogados')   
+    usuarioId = db.Column(db.Integer, db.ForeignKey('usuarios.id'))
+    usuario = db.relationship('Usuarios', backref='usuarios_advogados')
+    
+    
+class Advogados(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    email = db.Column(db.String(50), nullable=False)
+    lingua = db.Column(db.String(20), nullable=False)
+    senha = db.Column(db.String(15), nullable=False)
     nome = db.Column(db.String(80), nullable=False)
     telefone = db.Column(db.String(10), nullable=False)
     
-class Ongs(db.model):
+class Ongs(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     nome = db.Column(db.String(80), nullable=False)
     endereco = db.Column(db.String(120), nullable=False)
     
-class Empresas(db.model):
+class Empresas(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     nome = db.Column(db.String(80), nullable=False)
     salario = db.Column(db.Float(4.2), nullable=False)
@@ -40,23 +51,29 @@ class Empresas(db.model):
     contato = db.Column(db.String(20), nullable=False)
 
 class Advogados_Ongs(db.Model):
-    advogado_email = db.Column(db.String(50), db.ForeignKey('advogados.email'), nullable=False)
-    advogado = db.relationship('Advogados', backref='ongs')
-    ong_id = db.Column(db.Integer, db.ForeignKey('ong.id'), nullable=False)
-    ong = db.relationship('Ongs', backref='advogados')
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    advogado_email = db.Column(db.String(50), db.ForeignKey('advogados.email'))
+    advogado = db.relationship('Advogados', backref='advogados')
+    ong_id = db.Column(db.Integer, db.ForeignKey('ongs.id'))
+    ong = db.relationship('Ongs', backref='ongs')
     
 class Empresas_Usuarios(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     empresa_id = db.Column(db.Integer, db.ForeignKey('empresas.id'), nullable=False)
-    usuario_email = db.Column(db.String(50), db.ForeignKey('usuario.email'), nullable=False)
-    usuario = db.relationship('Usuarios', backref='empresas')
-    empresa = db.relationship('Empresas', backref='usuarios')
+    empresa = db.relationship('Empresas', backref='empresas')
+    usuarioId = db.Column(db.Integer, db.ForeignKey('usuarios.id'))
+    usuario = db.relationship('Usuarios', backref='usuarios')
         
-@app.route('/index', methods=['GET'])
+with app.app_context():
+    db.create_all()
+    
+@app.route('/', methods=['GET'])
 def index():
-    return render_template('.html')
+    return render_template('index.html')
 
 @app.route('/cadastro/usuario', methods=['GET', 'POST'])
 def cadastroUsuario():
+    # advogado = Advogados.query.all()
     if request.method == 'POST':
         email = request.form['email']
         nome = request.form['nome']
@@ -67,7 +84,7 @@ def cadastroUsuario():
         db.session.add(Usuarios(email=email, nome=nome, lingua=lingua, senha=senha, telefone=telefone, endereco=endereco))
         db.session.commit()
         return redirect('/')
-    return render_template('.html')
+    return render_template('criarUsuario.html')
 
 @app.route('/cadastro/advogado', methods=['GET', 'POST'])
 def cadastroAdvogado():
@@ -106,7 +123,7 @@ def cadastroEmpresa():
 @app.route('/lista/usuarios', methods=['GET'])
 def listaUsuarios():
     usuarios = Usuarios.query.all()
-    return render_template('.html', usuarios=usuarios)
+    return render_template('listarUsuario.html', usuarios=usuarios)
 
 @app.route('/lista/advogados', methods=['GET'])
 def listaAdvogados():
@@ -123,7 +140,83 @@ def listaEmpresas():
     empresas = Empresas.query.all()
     return render_template('.html', empresas=empresas)
 
+@app.route('/edit/usuario/<email>', methods=['GET', 'POST'])
+def editUsuario(email):
+    usuario = Usuarios.query.get_or_404(email)
+    advogado = Advogados.query.all()
+    if request.method == 'POST':
+        usuario.nome = request.form['nome']
+        usuario.lingua = request.form['lingua']
+        usuario.email = request.form['email']
+        usuario.senha = request.form['senha']
+        usuario.telefone = request.form['telefone']
+        usuario.endereco = request.form['endereco']
+        usuario.advogado.email = request.form['advogado_email']
+        db.session.commit()
+        return redirect('/lista/usuarios')
+    return render_template('editarUsuario.html', usuario=usuario, advogado=advogado)
 
+@app.route('/edit/advogado/<email>', methods=['GET', 'POST'])
+def editAdvogado(email):
+    advogado = Advogados.query.get_or_404(email)
+    if request.method == 'POST':
+        advogado.nome = request.form['nome']
+        advogado.email = request.form['email']
+        advogado.telefone = request.form['telefone']
+        db.session.commit()
+        return redirect('/lista/advogados')
+    return render_template('.html', advogado=advogado)
+
+@app.route('/edit/ong/<int:id>', methods=['GET', 'POST'])
+def editOng(id):
+    ong = Ongs.query.get_or_404(id)
+    if request.method == 'POST':
+        ong.nome = request.form['nome']
+        ong.endereco = request.form['endereco']
+        db.session.commit()
+        return redirect('/lista/ongs')
+    return render_template('.html', ong=ong)
+
+@app.route('/edit/empresa/<int:id>', methods=['GET', 'POST'])
+def editEmpresa(id):
+    empresa = Empresas.query.get_or_404(id)
+    if request.method == 'POST':
+        empresa.nome = request.form['nome']
+        empresa.salario = float(request.form['salario'])
+        empresa.especificacoes = request.form['especificacoes']
+        empresa.endereco = request.form['endereco']
+        empresa.contato = request.form['contato']
+        db.session.commit()
+        return redirect('/lista/empresas')
+    return render_template('.html', empresa=empresa)
+
+@app.route('/delete/usuario/<email>', methods=['GET'])
+def deleteUsuario(email):
+    usuario = Usuarios.query.get_or_404(email)
+    db.session.delete(usuario)
+    db.session.commit()
+    return redirect('/lista/usuarios')
+
+@app.route('/delete/advogado/<email>', methods=['GET'])
+def deleteAdvogado(email):
+    advogado = Advogados.query.get_or_404(email)
+    db.session.delete(advogado)
+    db.session.commit()
+    return redirect('/lista/advogados')
+
+@app.route('/delete/ong/<int:id>', methods=['GET'])
+def deleteOng(id):
+    ong = Ongs.query.get_or_404(id)
+    db.session.delete(ong)
+    db.session.commit()
+    return redirect('/lista/ongs')
+
+@app.route('/delete/empresa/<int:id>', methods=['GET'])
+def deleteEmpresa(id):
+    empresa = Empresas.query.get_or_404(id)
+    db.session.delete(empresa)
+    db.session.commit()
+    return redirect('/lista/empresas')
 
 if __name__ == '__main__':
     app.run(debug=True)
